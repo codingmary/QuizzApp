a7c9acdd8d920785a433753efdea1c84bc788d0a
MaryFili <filardimary@gmail.com>
2023-05-30 13:20:44 +0200

    header added and styled

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import { Header } from './components/Header'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Header />
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom'
+import './Header.scss'
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <div className='Title'>
+                <Link to="/"> <h1>Quiz Up
+                    <div className="Title__highlight"></div>
+                </h1>
+                </Link>
+                <div className="Title__underline"></div>
+                <div aria-hidden className="Title__filled">Quiz Up</div>
+            </div>
+        </div>
+    )
+}
